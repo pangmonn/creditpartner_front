4fe1067d8b334b8c1fd70362bb6c0c86489cef15
monn <71128541+pangmonn@users.noreply.github.com>
2023-07-30 11:11:06 +0900

    credit api 8080

--- src/components/credits/api/creditsAPI.js
+++ src/components/credits/api/creditsAPI.js
@@ -5,21 +5,40 @@ const client = axios.create();
 
 const config = { headers : { "Content-Type": 'application/json' } };
 
-let url;
+const semesterURL1 = 'http://localhost:8080/credits1';
+const semesterURL2 = 'http://localhost:8080/credits2';
+const semesterURL3 = 'http://localhost:8080/credits3';
+const semesterURL4 = 'http://localhost:8080/credits4';
+const semesterURL5 = 'http://localhost:8080/credits5';
+const semesterURL6 = 'http://localhost:8080/credits6';
+
 
 export const getClasses = (semester) => {
+    let URL;
     switch(semester) {
-        case '1':
-            url = 'http://localhost:3000/credits1';
+        case 1:
+            URL = semesterURL1;
             break;
-        case '2':
-            url = 'http://localhost:3000/credits2';
+        case 2:
+            URL = semesterURL2;
+            break;
+        case 3:
+            URL = semesterURL3;
+            break;
+        case 4:
+            URL = semesterURL4;
+            break;
+        case 5:
+            URL = semesterURL5;
+            break;
+        case 6:
+            URL = semesterURL6;
             break;
         default:
             break;
     }
     return new Promise((resolve, reject) => {
-        client.get(url, config)
+        client.get(URL, config)
             .then(response => {
                 // console.log(response.data.items);
                 resolve(response.data.items);
@@ -32,27 +51,35 @@ export const getClasses = (semester) => {
 
 export const postClasses = (form, semester) => {
     const data = { items: form };
-    switch (semester) {
-        case '1':
-            client.post('http://localhost:3000/credits1', data, config).then(function (response) {
-                console.log(response);
-                return response;
-            }).catch(function (error) {
-            console.log(error);
-            return error;
-            });
+    let URL;
+    switch(semester) {
+        case 1:
+            URL = semesterURL1;
             break;
-        case '2':
-            client.post('http://localhost:3000/credits2', data, config).then(function (response) {
-                console.log(response);
-                return response;
-            }).catch(function (error) {
-            console.log(error);
-            return error;
-            });
+        case 2:
+            URL = semesterURL2;
+            break;
+        case 3:
+            URL = semesterURL3;
+            break;
+        case 4:
+            URL = semesterURL4;
+            break;
+        case 5:
+            URL = semesterURL5;
+            break;
+        case 6:
+            URL = semesterURL6;
             break;
         default:
             break;
     }
+    client.post(URL, data, config).then(function (response) {
+            console.log(response);
+            return response;
+            }).catch(function (error) {
+                console.log(error);
+                return error;
+                });
     
 }
