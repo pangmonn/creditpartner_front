2a9ecb6271b460a2fe25bd4eb9f1e3a18a10bd95
seojunP <sjsjwest6@gmail.com>
2023-08-16 17:16:48 +0900

    230816_majorPopUp Design

--- src/components/major/MajorPopUp.js
+++ src/components/major/MajorPopUp.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Modal from 'react-modal';
+import './styles/majorpopup.css'
 // import departmentDetail from './departmentDetail.json'
 
 const MajorPopUp = ({descript, recommend, similar, general_credit, career_credit}) => {
@@ -15,50 +16,53 @@ const MajorPopUp = ({descript, recommend, similar, general_credit, career_credit
     return (
         <div>
             <button onClick={()=> setModalIsOpen(true)}>학과 정보</button>
-            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+            <Modal className="majorPopUpContainer" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
                     <div>
-                        <div>- 학과 설명</div>
-                        <div>
-                            {descript}
-                        </div>
-                        <br />
-
-                        <div>- 이런 학생에게 추천해요</div>
-                        <ul>
-                            {recommend.map((item, index) => (
-                                <li key={index}>{item}</li>
-                            ))}
-                        </ul>
-                        <br />
-
-                        <div>- 유사 학과</div>
-                        <ul>
-                            {similar.map((item, index) => (
-                                <li key={index}>{item}</li>
-                            ))}
-                        </ul>
-                        <br />
-
-                        <div>
-                        - 추천하는 선택 과목
+                        <div>학과명</div>
+                        <div className="majorPopUpContent">
+                            <div>- 학과 설명</div>
                             <div>
-                                ... 일반 선택
+                                {descript}
                             </div>
+                            <br />
+
+                            <div>- 이런 학생에게 추천해요</div>
                             <ul>
-                                {general_credit.map((item, index) => (
+                                {recommend.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                            </ul>
+                            <br />
+
+                            <div>- 유사 학과</div>
+                            <ul>
+                                {similar.map((item, index) => (
                                     <li key={index}>{item}</li>
                                 ))}
                             </ul>
                             <br />
 
                             <div>
-                                ... 진로 선택
+                            - 추천하는 선택 과목
+                                <div>
+                                    ... 일반 선택
+                                </div>
+                                <ul>
+                                    {general_credit.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
+                                </ul>
+                                <br />
+
+                                <div>
+                                    ... 진로 선택
+                                </div>
+                                <ul>
+                                    {career_credit.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
+                                </ul>
                             </div>
-                            <ul>
-                                {career_credit.map((item, index) => (
-                                    <li key={index}>{item}</li>
-                                ))}
-                            </ul>
                         </div>
 
                         <button onClick={modalClose}>닫기</button>
