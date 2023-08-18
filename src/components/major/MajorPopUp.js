f4c0590e0d8cbb692fb5820737420bca2f2cc36f
seojunP <sjsjwest6@gmail.com>
2023-08-18 12:47:00 +0900

    230818_majorPopUp Design

--- src/components/major/MajorPopUp.js
+++ src/components/major/MajorPopUp.js
@@ -18,15 +18,18 @@ const MajorPopUp = ({descript, recommend, similar, general_credit, career_credit
             <button onClick={()=> setModalIsOpen(true)}>학과 정보</button>
             <Modal className="majorPopUpContainer" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
                     <div>
-                        <div>학과명</div>
+                        <div className="majorPopUpTop">
+                            <div>학과명</div>
+                            <button className="majorPopUpClose" onClick={modalClose}>닫기</button>
+                        </div>
                         <div className="majorPopUpContent">
-                            <div>- 학과 설명</div>
+                            <div className="majorPopUpTitle">- 학과 설명</div>
                             <div>
                                 {descript}
                             </div>
                             <br />
 
-                            <div>- 이런 학생에게 추천해요</div>
+                            <div className="majorPopUpTitle">- 이런 학생에게 추천해요</div>
                             <ul>
                                 {recommend.map((item, index) => (
                                     <li key={index}>{item}</li>
@@ -34,7 +37,7 @@ const MajorPopUp = ({descript, recommend, similar, general_credit, career_credit
                             </ul>
                             <br />
 
-                            <div>- 유사 학과</div>
+                            <div className="majorPopUpTitle">- 유사 학과</div>
                             <ul>
                                 {similar.map((item, index) => (
                                     <li key={index}>{item}</li>
@@ -43,8 +46,10 @@ const MajorPopUp = ({descript, recommend, similar, general_credit, career_credit
                             <br />
 
                             <div>
-                            - 추천하는 선택 과목
-                                <div>
+                            <div className="majorPopUpTitle">
+                                - 추천하는 선택 과목
+                            </div>
+                                <div className="majorPopUpTitle">
                                     ... 일반 선택
                                 </div>
                                 <ul>
@@ -54,7 +59,7 @@ const MajorPopUp = ({descript, recommend, similar, general_credit, career_credit
                                 </ul>
                                 <br />
 
-                                <div>
+                                <div className="majorPopUpTitle">
                                     ... 진로 선택
                                 </div>
                                 <ul>
@@ -64,8 +69,6 @@ const MajorPopUp = ({descript, recommend, similar, general_credit, career_credit
                                 </ul>
                             </div>
                         </div>
-
-                        <button onClick={modalClose}>닫기</button>
                     </div>
                 </Modal>
         </div>
