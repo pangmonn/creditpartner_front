5404cd220ef5c0722416d21924a3cfc2ecf85708
monn <71128541+pangmonn@users.noreply.github.com>
2023-08-20 21:39:22 +0900

    0820 chatbot

--- src/components/major/MajorPopUp.js
+++ src/components/major/MajorPopUp.js
@@ -1,40 +1,76 @@
 import React, {useState} from "react";
 import Modal from 'react-modal';
+import './styles/majorpopup.css'
+// import departmentDetail from './departmentDetail.json'
 
-const MajorPopUp = () => {
+const MajorPopUp = ({descript, recommend, similar, general_credit, career_credit}) => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
+    // const matchingDepartment = departmentDetail.find((department) => department.majorNum === majorNum);
+    // console.log(departmentDetail);
+
+    const modalClose = () => {
+    	setModalIsOpen(!modalIsOpen);
+  	};
     
+
     return (
         <div>
             <button onClick={()=> setModalIsOpen(true)}>학과 정보</button>
-            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-                <div>
-                - 학과 설명
-                </div>
-                <br />
-
-                <div>
-                - 이런 학생에게 추천해요  
-                </div>
-                <br />
-
-                <div>
-                - 유사 학과
-                </div>
-                <br />
-
-                <div>
-                - 추천하는 선택 과목
+            <Modal className="majorPopUpContainer" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
                     <div>
-                        ... 일반 선택
-                    </div>
-                    <br />
+                        <div className="majorPopUpTop">
+                            <div>학과명</div>
+                            <button className="majorPopUpClose" onClick={modalClose}>닫기</button>
+                        </div>
+                        <div className="majorPopUpContent">
+                            <div className="majorPopUpTitle">- 학과 설명</div>
+                            <div>
+                                {descript}
+                            </div>
+                            <br />
 
-                    <div>
-                        ... 진로 선택
+                            <div className="majorPopUpTitle">- 이런 학생에게 추천해요</div>
+                            <ul>
+                                {recommend.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                            </ul>
+                            <br />
+
+                            <div className="majorPopUpTitle">- 유사 학과</div>
+                            <ul>
+                                {similar.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                            </ul>
+                            <br />
+
+                            <div>
+                            <div className="majorPopUpTitle">
+                                - 추천하는 선택 과목
+                            </div>
+                                <div className="majorPopUpTitle">
+                                    ... 일반 선택
+                                </div>
+                                <ul>
+                                    {general_credit.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
+                                </ul>
+                                <br />
+
+                                <div className="majorPopUpTitle">
+                                    ... 진로 선택
+                                </div>
+                                <ul>
+                                    {career_credit.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </Modal>
+                </Modal>
         </div>
     )
 }
