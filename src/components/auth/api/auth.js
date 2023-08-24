6e82ffb02e9d523db2eaf94a842c8b89f7e4fb4f
monn <71128541+pangmonn@users.noreply.github.com>
2023-08-24 20:18:08 +0900

    0824 login

--- src/components/auth/api/auth.js
+++ src/components/auth/api/auth.js
@@ -2,14 +2,16 @@ import client from './client';
 
 const config = { headers : { "Content-Type": 'application/json' } };
 
-export const login = ({ userId, password }) =>
-    client.post('http://localhost:8080/login', { id: userId, pwd: password}).then(function (response) {
+export const login = ({ userId, password }) => {
+    const data = { 'id': userId, 'pwd': password };
+    client.post('api/login', data, config).then(function (response) {
         console.log(response);
         return response;
     }).catch(function (error) {
         console.log(error);
         console.log(error.response.status);
     });
+}
 
 export const join = (form) => {
     const data = { 'name': form.userName, 'id': form.userId, 'pwd': form.password };
@@ -21,5 +23,3 @@ export const join = (form) => {
         return error;
     });
 }
-
-export const check = () => client.get('/api/auth/check');
