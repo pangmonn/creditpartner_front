9e1a83dba4460da2b3f9fdeb53cbb8ac076e696a
seojunP <sjsjwest6@gmail.com>
2023-11-06 14:19:54 +0900

    231106_main menu about us Link

--- src/components/main/Menu.js
+++ src/components/main/Menu.js
@@ -4,7 +4,7 @@ import './styles/menu.css'
 const Menu = () => {
     return (
         <div className='menu'>
-            <span className='menus'>about us</span>
+            <span className='menus'><a href="https://silky-equinox-081.notion.site/About-us-44952eaf0e6541e0b4a130a23d8b27b4" target="_blank" rel="noopener noreferrer">about us</a></span>
             <span className='menus'>contact</span>
         </div>
     );
