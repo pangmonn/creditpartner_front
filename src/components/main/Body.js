aad5d429c4100d496237a885df79c42338a4f9ea
seojunP <sjsjwest6@gmail.com>
2023-11-07 16:21:57 +0900

    231107_main design

--- src/components/main/Body.js
+++ src/components/main/Body.js
@@ -9,7 +9,15 @@ const Body = () => {
         <div className='body'>
             <div className='box1'>
                 <div className='boxTitle'>We'll be your <span className='colorBlue'> PARTNER</span></div>
-                <div className='boxText'>크레딧 파트너는 2025년부터 시행되는 고교학점제에 대한 학생들의 혼란을 줄이고 과목, 진로 선택에 도움을 주기 위해 희망 진로에 따라 맞춤형 서비스를 제공합니다.</div>
+                <div className='boxText'>
+                    크레딧 파트너는 2025년부터 시행되는
+                    <br/>
+                    고교학점제에 대한 학생들의 혼란을 줄이고
+                    <br/>
+                    과목, 진로 선택에 도움을 주기 위해
+                    <br/>
+                    희망 진로에 따라 맞춤형 서비스를 제공합니다.
+                    </div>
                 {
                     (window.localStorage.length===0) ? (
                         <>
