775de628e9ee9dfd399e57afa758b03a1a274a57
seojunP <sjsjwest6@gmail.com>
2023-11-07 15:25:47 +0900

    231107_major popup design

--- src/components/major/MajorPopUp.js
+++ src/components/major/MajorPopUp.js
@@ -47,66 +47,66 @@ const MajorPopUp = ({major_id, major_name, similar}) => {
         <div>
             <button className="majorPopUpButton" onClick={()=> setModalIsOpen(true)}>학과 정보</button>
             <Modal className="majorPopUpContainer" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                <div>
+                    <div className="majorPopUpTop">
+                        <div>{major_name}</div>
+                        <button className="majorPopUpClose" onClick={modalClose}>닫기</button>
+                    </div>
                     <div>
-                        <div className="majorPopUpTop">
-                            <div>{major_name}</div>
-                            <button className="majorPopUpClose" onClick={modalClose}>닫기</button>
+                        <div className="majorPopUpTitle">학과 설명</div>
+                        <div className="majorPopUpContent">
+                            {descript}
                         </div>
-                        <div>
-                            <div className="majorPopUpTitle">학과 설명</div>
-                            <div className="majorPopUpContent">
-                                {descript}
-                            </div>
-                            <br />
+                        <br />
 
-                            <div className="majorPopUpTitle">
-                                이런 학생에게 추천해요
+                        <div className="majorPopUpTitle">
+                            이런 학생에게 추천해요
+                            </div>
+                        <ul 
+                            className="majorPopUpContent-recommend"
+                            style={{listStyle: 'decimal', listStylePosition: 'inside'}}      
+                        >
+                            {recommend.map((item, index) => (
+                                <div>
+                                    <li key={index}>{item}</li>
+                                    <br/>
                                 </div>
+                            ))}
+                        </ul>
+                        <br />
+
+                        <div>
+                        <div className="majorPopUpTitle">
+                            추천하는 선택 과목
+                        </div>
+                            <div className="majorPopUpTitle-select">
+                                일반 선택
+                            </div>
                             <ul 
-                                className="majorPopUpContent-recommend"
-                                style={{listStyle: 'decimal', listStylePosition: 'inside'}}      
+                                className="majorPopUpContent-select"
+                                style={{listStyle: 'square', listStylePosition: 'inside'}}    
                             >
-                                {recommend.map((item, index) => (
-                                    <div>
-                                        <li key={index}>{item}</li>
-                                        <br/>
-                                    </div>
+                                {general_credit.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                                <br />
+                            </ul>
+
+                            <div className="majorPopUpTitle-select">
+                                진로 선택
+                            </div>
+                            <ul 
+                                className="majorPopUpContent-select"
+                                style={{listStyle: 'square', listStylePosition: 'inside'}}  
+                            >
+                                {career_credit.map((item, index) => (
+                                    <li key={index}>{item}</li>
                                 ))}
                             </ul>
-                            <br />
-
-                            <div>
-                            <div className="majorPopUpTitle">
-                                추천하는 선택 과목
-                            </div>
-                                <div className="majorPopUpTitle-select">
-                                    일반 선택
-                                </div>
-                                <ul 
-                                    className="majorPopUpContent-select"
-                                    style={{listStyle: 'square', listStylePosition: 'inside'}}    
-                                >
-                                    {general_credit.map((item, index) => (
-                                        <li key={index}>{item}</li>
-                                    ))}
-                                    <br />
-                                </ul>
-
-                                <div className="majorPopUpTitle-select">
-                                    진로 선택
-                                </div>
-                                <ul 
-                                    className="majorPopUpContent-select"
-                                    style={{listStyle: 'square', listStylePosition: 'inside'}}  
-                                >
-                                    {career_credit.map((item, index) => (
-                                        <li key={index}>{item}</li>
-                                    ))}
-                                </ul>
-                            </div>
                         </div>
                     </div>
-                </Modal>
+                </div>
+            </Modal>
         </div>
     )
 }
