265c5a5ef6c62cc95188fe34a1fdf2b2c95c209c
seojunP <sjsjwest6@gmail.com>
2023-11-14 16:05:02 +0900

    231114_support popup

--- src/components/guide/Top.js
+++ src/components/guide/Top.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import './styles/top.css'
-import home from '../../images/home.png'
+import SupportPopUp from './SupportPopUp';
+import './styles/top.css';
+import home from '../../images/home.png';
 import { Link } from 'react-router-dom';
 
 const imgstyle = {
     width: '30px',
     height: '30px',
     marginLeft: 'auto'
-}
+};
 
 const Top = () => {
     return (
         <div className='guideTop'>
-            <div className='guideTitle'>학과별 이수 가이드</div>
+            <div style={{display: 'flex'}}>
+                <div className='guideTitle'>학과별 이수 가이드</div>
+                <SupportPopUp />
+            </div>
+            
             <Link to='/'>
-                <img src={home} style={imgstyle} />
+                <img src={home} style={imgstyle} alt="Home" />
             </Link>
         </div>
     );
